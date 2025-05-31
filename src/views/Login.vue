4fe6fa39cb99a0bca103999dba9b879889c83671
KomangArmawan <komang.armawan.27@gmail.com>
2025-06-01 01:17:20 +0700

    update: handle error message on login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -28,6 +28,14 @@
             required
           />
         </div>
+
+        <div v-if="error" class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded text-sm">
+          <ul v-if="Array.isArray(error)">
+            <li v-for="(errMsg, idx) in error" :key="idx">• {{ errMsg }}</li>
+          </ul>
+          <p v-else>{{ error }}</p>
+        </div>
+        
         <div class="flex flex-col gap-2 justify-center items-center">
           <button
             type="submit"
